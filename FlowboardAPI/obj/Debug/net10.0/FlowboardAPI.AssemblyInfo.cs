955d0cec2f8653f9b97ee583d0341b9db1c3c91b
Oscar Lizandro Vasquez Llave <vasquezllaveoscarlizandro@gmail.com>
2026-06-16 18:57:09 -0500

    feat: delete trash files

--- FlowboardAPI/obj/Debug/net10.0/FlowboardAPI.AssemblyInfo.cs
+++ FlowboardAPI/obj/Debug/net10.0/FlowboardAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowboardAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75c14ba99dba6cda6c939d36f32c9040d7b166a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730c059054d135a5c63505c150ccac5974ad96a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowboardAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowboardAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
